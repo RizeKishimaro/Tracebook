3087683b25ef77c5ed3f0232c200f55382402531
Linus Walker <w000alker@gmail.com>
2023-01-30 21:59:31 +0630

    Added simple encoded jwt

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,15 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 mod scopes;
 use scopes::user::user_scope;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(user_scope()))
-        .bind(("127.0.0.1", 8090))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(String::from("secret")))
+            .service(user_scope())
+    })
+    .bind(("127.0.0.1", 8090))?
+    .run()
+    .await
 }
--- backend/src/scopes/user.rs
+++ backend/src/scopes/user.rs
@@ -1,14 +1,61 @@
 use actix_web::{web, HttpResponse, Scope};
+use chrono::{Duration, Utc};
+use jsonwebtoken::{encode, EncodingKey, Header};
+use serde::{Deserialize, Serialize};
 
 pub fn user_scope() -> Scope {
     web::scope("/user")
         .route("/encode-token/{id}", web::get().to(encode_token))
-        .route("decode-token", web::get().to(decode_token))
+        .route("decode-token", web::post().to(decode_token))
         .route("protected", web::get().to(protected))
 }
 
+#[derive(Serialize, Deserialize)]
+struct Claims {
+    id: usize,
+    exp: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct EncodeResponse {
+    message: String,
+    token: String,
+}
+
 async fn encode_token(path: web::Path<usize>, secret: web::Data<String>) -> HttpResponse {
+    let id: usize = path.into_inner();
+    let exp: usize = (Utc::now() + Duration::days(365)).timestamp() as usize;
+    let claim: Claims = Claims { id, exp };
+    let token: String = encode(
+        &Header::default(),
+        &claim,
+        &EncodingKey::from_secret(secret.as_str().as_ref()),
+    )
+    .unwrap();
+    HttpResponse::Ok().json(EncodeResponse {
+        message: String::from("success"),
+        token,
+    })
+}
+
+#[derive(Serialize, Deserialize)]
+struct DecodeBody {
+    token: String,
+}
+
+async fn decode_token(body: web::Json<DecodeBody>, secret: web::Data<String>) -> HttpResponse {
     HttpResponse::Ok().json(Response {
-        message: "encode_token".to_owned(),
+        message: String::from("decode-token"),
+    })
+}
+
+async fn protected() -> HttpResponse {
+    HttpResponse::Ok().json(Response {
+        message: String::from("protected"),
     })
 }
