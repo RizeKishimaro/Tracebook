af40f862e33d7cc2ed071f02592e766e44b8d817
Linus Walker <w000alker@gmail.com>
2023-01-31 21:12:14 +0630

    Simple jwt auth added

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+mod scopes;
+use scopes::user::user_scope;
+mod extractors;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(String::from(
+                "abcdefghijklmnopqrstuvwxyzwalkerize0123456789walkerizeABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            )))
+            .service(user_scope())
+    })
+    .bind(("127.0.0.1", 8090))?
+    .run()
+    .await
 }
