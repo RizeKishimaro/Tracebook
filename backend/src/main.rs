fc869d9124eb344211098d13e1ae7dd7ee3dec47
Linus Walker <w000alker@gmail.com>
2023-01-31 17:26:06 +0630

    Start making jwt auth

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,3 @@
-use actix_web::{web, App, HttpServer};
-mod scopes;
-use scopes::user::user_scope;
-mod extractors;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .app_data(web::Data::new(String::from("secret")))
-            .service(user_scope())
-    })
-    .bind(("127.0.0.1", 8090))?
-    .run()
-    .await
+fn main() {
+    println!("Hello, world!");
 }
