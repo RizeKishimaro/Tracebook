7891f089ea31b3fc3de040ab8548bc182ff757f9
Linus Walker <w000alker@gmail.com>
2023-02-03 20:44:34 +0630

    Added Basic Login Page with rust

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,4 @@
+use actix_cors::Cors;
 use actix_web::{web, App, HttpResponse, HttpServer};
 mod scopes;
 use scopes::user::user_scope;
@@ -6,7 +7,9 @@ mod extractors;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::permissive();
         App::new()
+            .wrap(cors)
             .app_data(web::Data::new(String::from(
                 "abcdefghijklmnopqrstuvwxyzwalkerize0123456789walkerizeABCDEFGHIJKLMNOPQRSTUVWXYZ",
             )))
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,20 +1,63 @@
-use gloo::console::log;
+use gloo::{console::log, dialogs::alert};
+use reqwasm::http::Request;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use wasm_bindgen_futures::spawn_local;
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
+#[derive(Deserialize, Serialize)]
+struct Response {
+    info: String,
+    message: String,
+    token: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct LoginData {
+    data: Response,
+}
+
+async fn login(username: String, password: String) -> Response {
+    let data = json!({
+        "username": username,
+        "password": password,
+    });
+
+    let response = Request::post("http://localhost:8090/user/encode-token")
+        .header("content-type", "application/json")
+        .body(data.to_string())
+        .send()
+        .await
+        .unwrap()
+        .json::<LoginData>()
+        .await
+        .unwrap();
+    response.data
+}
+
 #[function_component]
 fn MyComponent() -> Html {
-    let input_value_handle = use_state(String::default);
-    let input_value = (*input_value_handle).clone();
+    let name_input_value_handle = use_state(String::default);
+    let name_input_value = (*name_input_value_handle).clone();
+    let pass_input_value_handle = use_state(String::default);
+    let pass_input_value = (*pass_input_value_handle).clone();
 
-    let on_cautious_change = {
-        let input_value_handle = input_value_handle.clone();
+    let on_submit = {
+        spawn_local(async move {
+            let res = login(name_input_value.clone(), pass_input_value.clone()).await;
+            alert(&res.token);
+        })
+    };
+
+    let on_name_change = {
+        let name_input_value_handle = name_input_value_handle.clone();
 
         Callback::from(move |e: Event| {
             let input = e.target_dyn_into::<HtmlInputElement>();
 
             if let Some(input) = input {
-                input_value_handle.set(input.value().clone());
+                name_input_value_handle.set(input.value().clone());
                 log!(input.value());
             }
         })
@@ -23,17 +66,16 @@ fn MyComponent() -> Html {
     let on_dangerous_change = Callback::from(move |e: Event| {
         // You must KNOW target is a HtmlInputElement, otherwise
         // the call to value would be Undefined Behaviour (UB).
-        input_value_handle.set(e.target_unchecked_into::<HtmlInputElement>().value());
+        pass_input_value_handle.set(e.target_unchecked_into::<HtmlInputElement>().value());
     });
 
     html! {
         <>
             <label for="cautious-input">
                 { "My cautious input:" }
-                <input onchange={on_cautious_change}
+                <input onchange={on_name_change}
                     id="cautious-input"
                     type="text"
-                    value={input_value.clone()}
                 />
             </label>
             <label for="dangerous-input">
@@ -41,9 +83,9 @@ fn MyComponent() -> Html {
                 <input onchange={on_dangerous_change}
                     id="dangerous-input"
                     type="text"
-                    value={input_value}
                 />
             </label>
+            <button onclick={Callback::from(move |_| on_submit)}>{"Click Me!"}</button>
         </>
     }
 }
