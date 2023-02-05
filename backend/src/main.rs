66f8ea6b4deae3a0e60332ad36fae6faa3f417f6
Linus Walker <w000alker@gmail.com>
2023-02-05 12:16:29 +0630

    making new user

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,24 +1,15 @@
-use std::future::Future;
-
 use actix_cors::Cors;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 mod scopes;
 use scopes::user::user_scope;
-use surrealdb::{Datastore, Session};
 mod extractors;
-
-struct DB {
-    ds: Datastore,
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut db: &Future<Output = Datastore> = &gtk();
     HttpServer::new(|| {
         let cors = Cors::permissive();
         App::new()
             .wrap(cors)
-            .app_data((Session::for_db("rep", "opb")))
             .app_data(web::Data::new(String::from(
                 "abcdefghijklmnopqrstuvwxyzwalkerize0123456789walkerizeABCDEFGHIJKLMNOPQRSTUVWXYZ",
             )))
@@ -27,8 +18,4 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8090))?
     .run()
     .await
-}
-
-async fn gtk() -> Datastore {
-    Datastore::new("memory").await.unwrap()
 }
