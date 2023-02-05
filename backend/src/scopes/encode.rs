abe716bfe47354c684e0ad42a37a42c546980fb8
Linus Walker <w000alker@gmail.com>
2023-02-05 15:30:24 +0630

    rewritted simple encode login func

--- backend/src/scopes/encode.rs
+++ backend/src/scopes/encode.rs
@@ -1,9 +1,26 @@
-use crate::scopes::user::{Claims, Info};
+use crate::scopes::user::{Claims, EncodeResponse, Info};
 use actix_web::{web, HttpResponse};
+use jsonwebtoken::{encode, EncodingKey, Header};
 use rand::random;
 
 pub async fn encode_token(body: web::Json<Info>, secret: web::Data<String>) -> HttpResponse {
-    let id = format!("{}{}", random::<u32>(), body.username);
+    let id = format!("{}{}", random::<u32>(), body.username.clone());
 
-    HttpResponse::Ok().await.unwrap()
+    let claim: Claims = Claims {
+        id,
+        username: body.username.clone(),
+        password: body.password.clone(),
+    };
+
+    let token: String = encode(
+        &Header::default(),
+        &claim,
+        &EncodingKey::from_secret(secret.as_str().as_ref()),
+    )
+    .unwrap();
+
+    HttpResponse::Ok().json(EncodeResponse {
+        message: String::from("success"),
+        token,
+    })
 }
--- backend/src/scopes/user.rs
+++ backend/src/scopes/user.rs
@@ -1,7 +1,9 @@
-use crate::extractors::auth_token::AuthToken;
 use crate::scopes::encode::encode_token;
-use actix_web::{web, Scope};
+use actix_web::{web, HttpResponse, Scope};
 use serde::{Deserialize, Serialize};
+use surrealdb::{Datastore, Session};
+
+type DB = (Datastore, Session);
 
 #[derive(Serialize, Deserialize)]
 pub struct Claims {
@@ -23,5 +25,22 @@ pub struct Info {
 }
 
 pub fn user_scope() -> Scope {
-    web::scope("/user").route("encode-token", web::post().to(encode_token))
+    web::scope("/user").route("{method}", web::post().to(branch))
+}
+
+pub async fn branch(
+    method: web::Path<String>,
+    body: web::Json<Info>,
+    secret: web::Data<String>,
+) -> HttpResponse {
+    let db = &(
+        Datastore::new("memory").await.unwrap(),
+        Session::for_db("trace", "book"),
+    );
+    let (ds, ses) = db;
+    if method.as_str() == "encode-token" {
+        encode_token(body, secret).await
+    } else {
+        HttpResponse::BadRequest().await.unwrap()
+    }
 }
