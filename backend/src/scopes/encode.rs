f29f6d303237a2b73894d3a346ec06f3310ddebb
Linus Walker <w000alker@gmail.com>
2023-02-05 15:10:45 +0630

    rewriting encode func

--- backend/src/scopes/encode.rs
+++ backend/src/scopes/encode.rs
@@ -1,6 +1,9 @@
-use crate::scopes::user;
-use actix_web::HttpResponse;
+use crate::scopes::user::{Claims, Info};
+use actix_web::{web, HttpResponse};
+use rand::random;
 
-pub async fn encode_token() -> HttpResponse {
+pub async fn encode_token(body: web::Json<Info>, secret: web::Data<String>) -> HttpResponse {
+    let id = format!("{}{}", random::<u32>(), body.username);
+
     HttpResponse::Ok().await.unwrap()
 }
--- backend/src/scopes/user.rs
+++ backend/src/scopes/user.rs
@@ -1,6 +1,26 @@
 use crate::extractors::auth_token::AuthToken;
 use crate::scopes::encode::encode_token;
 use actix_web::{web, Scope};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Claims {
+    pub id: String,
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct EncodeResponse {
+    pub message: String,
+    pub token: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Info {
+    pub username: String,
+    pub password: String,
+}
 
 pub fn user_scope() -> Scope {
     web::scope("/user").route("encode-token", web::post().to(encode_token))
