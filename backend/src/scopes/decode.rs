68768ab14da341d970b0d9986dd71348a2c5bf37
Linus Walker <w000alker@gmail.com>
2023-02-05 16:29:21 +0630

    added decode func

--- backend/src/scopes/decode.rs
+++ backend/src/scopes/decode.rs
@@ -1,2 +1,25 @@
-use crate::scopes::encode;
+use actix_web::{web, HttpResponse};
+use jsonwebtoken::{decode, errors::Error, DecodingKey, TokenData, Validation};
+
+use super::user::{Claims, DecodeBody, DecodeResponse, Response};
+
+pub async fn decode_token(body: web::Json<DecodeBody>, secret: web::Data<String>) -> HttpResponse {
+    let decoded: Result<TokenData<Claims>, Error> = decode(
+        &body.token,
+        &DecodingKey::from_secret(secret.as_str().as_ref()),
+        &Validation::new(jsonwebtoken::Algorithm::HS256),
+    );
+
+    match decoded {
+        Ok(token) => HttpResponse::Ok().json(DecodeResponse {
+            message: "Authed".to_string(),
+            id: token.claims.id,
+            username: token.claims.username,
+            password: token.claims.password,
+        }),
+        Err(e) => HttpResponse::BadRequest().json(Response {
+            message: e.to_string(),
+        }),
+    }
+}
 
--- backend/src/scopes/user.rs
+++ backend/src/scopes/user.rs
@@ -3,6 +3,8 @@ use actix_web::{web, HttpResponse, Scope};
 use serde::{Deserialize, Serialize};
 use surrealdb::{Datastore, Session};
 
+use super::decode::decode_token;
+
 pub type DB = (Datastore, Session);
 
 #[derive(Serialize, Deserialize)]
@@ -24,12 +26,31 @@ pub struct Info {
     pub password: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct DecodeBody {
+    pub token: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Response {
+    pub message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DecodeResponse {
+    pub message: String,
+    pub id: String,
+    pub username: String,
+    pub password: String,
+}
+
 pub fn user_scope() -> Scope {
     web::scope("/user").route("{method}", web::post().to(branch))
 }
 
 pub async fn branch(
     method: web::Path<String>,
+    debody: web::Json<DecodeBody>,
     body: web::Json<Info>,
     secret: web::Data<String>,
 ) -> HttpResponse {
@@ -39,6 +60,8 @@ pub async fn branch(
     );
     if method.as_str() == "encode-token" {
         encode_token(db, body, secret).await
+    } else if method.as_str() == "decode-token" {
+        decode_token(debody, secret).await
     } else {
         HttpResponse::BadRequest().await.unwrap()
     }
