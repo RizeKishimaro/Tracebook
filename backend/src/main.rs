5645211d28b41bcbd410de37a49b3bf89b765a8a
Linus Walker <w000alker@gmail.com>
2023-02-06 18:27:40 +0630

    added .env secret key for jwt auth safty

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,5 @@
 use actix_cors::Cors;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 mod scopes;
 use dotenvy::dotenv;
 use scopes::user::user_scope;
@@ -10,9 +10,10 @@ async fn main() -> std::io::Result<()> {
     dotenv().expect("Error due to: .env File not found");
     HttpServer::new(|| {
         let cors = Cors::permissive();
+        let l = dotenvy::var("SECRET").unwrap();
         App::new()
             .wrap(cors)
-            .app_data(dotenvy::var("SECRET").unwrap())
+            .app_data(web::Data::new(l))
             .service(user_scope())
     })
     .bind(("127.0.0.1", 8090))?
