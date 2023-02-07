74a03385fadeb241937a7ae40453681eb9b9f149
Linus Walker <w000alker@gmail.com>
2023-02-07 09:18:47 +0630

    added extractor for db responses

--- backend/src/scopes/into_obj.rs
+++ backend/src/scopes/into_obj.rs
@@ -1,1 +1,25 @@
-fn into_obj() {}
+use anyhow::anyhow;
+use surrealdb::sql::Value::{Array, Object};
+use surrealdb::Response;
+
+fn into_obj(
+    ress: Vec<Response>,
+) -> Result<impl Iterator<Item = Result<surrealdb::sql::Object, anyhow::Error>>, anyhow::Error> {
+    let res = ress
+        .into_iter()
+        .next()
+        .map(|rp| rp.result)
+        .transpose()
+        .unwrap();
+
+    match res {
+        Some(Array(arr)) => {
+            let resul = arr.into_iter().map(|v| match v {
+                Object(obj) => Ok(obj),
+                _ => Err(anyhow!("Invalid record!")),
+            });
+            Ok(resul)
+        }
+        _ => Err(anyhow!("No record found!")),
+    }
+}
