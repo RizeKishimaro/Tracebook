95b5d5eaee10fc5f5bb428c9f7f93bd14156785a
Linus Walker <w000alker@gmail.com>
2023-02-08 20:44:39 +0630

    fixed errors in normal login check

--- backend/src/scopes/normal_login.rs
+++ backend/src/scopes/normal_login.rs
@@ -4,9 +4,11 @@ use super::user::{Info, DB};
 
 pub async fn login((ds, ses): &DB, body: web::Json<Info>) -> HttpResponse {
     let sql = format!(
-        "SELECT * FROM user WHERE (emnum = \"{}\", username = \"{}\");",
+        "SELECT * FROM user WHERE emnum = \"{}\" AND username = \"{}\" AND password = \"{}\" AND sex = \"{}\";",
         body.emnum.clone(),
-        body.username.clone()
+        body.username.clone(),
+        body.password.clone(),
+        body.sex.clone()
     );
 
     let resul = ds.execute(&sql, ses, None, false).await.unwrap();
