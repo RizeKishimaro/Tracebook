0a5d13f59eaf43a4884fab7e236a93243ab37ee2
Linus Walker <w000alker@gmail.com>
2023-02-08 20:27:42 +0630

    checked for normal login

--- backend/src/scopes/normal_login.rs
+++ backend/src/scopes/normal_login.rs
@@ -3,7 +3,11 @@ use actix_web::{web, HttpResponse};
 use super::user::{Info, DB};
 
 pub async fn login((ds, ses): &DB, body: web::Json<Info>) -> HttpResponse {
-    let sql = format!("SELECT * FROM user WHERE (emnum = {});", body.emnum.clone());
+    let sql = format!(
+        "SELECT * FROM user WHERE (emnum = \"{}\", username = \"{}\");",
+        body.emnum.clone(),
+        body.username.clone()
+    );
 
     let resul = ds.execute(&sql, ses, None, false).await.unwrap();
 
