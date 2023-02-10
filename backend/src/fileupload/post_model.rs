23ddc14ea82bb0313471637792014f5f6fd0aa5c
Linus Walker <w000alker@gmail.com>
2023-02-10 16:52:52 +0630

    making db tablr

--- backend/src/fileupload/post_model.rs
+++ backend/src/fileupload/post_model.rs
@@ -1,9 +1,28 @@
+use std::collections::BTreeMap;
+
 use actix_web::{web, HttpResponse};
+use jsonwebtoken::{decode, errors::Error, DecodingKey, TokenData, Validation};
 use rand::random;
+use surrealdb::sql::Value;
+
+use crate::scopes::user::Claims;
 
 use super::upload_sc::Model;
 
-pub async fn post(model: web::Json<Model>) -> HttpResponse {
-    let post_id = random::<u128>();
+pub async fn post(model: web::Json<Model>, secret: web::Data<String>) -> HttpResponse {
+    let post_id = random::<u64>();
+    let user_info: Result<TokenData<Claims>, Error> = decode(
+        &model.user_token,
+        &DecodingKey::from_secret(secret.as_str().as_ref()),
+        &Validation::new(jsonwebtoken::Algorithm::HS256),
+    );
+    let sql = format!("CREATE post:{post_id} CONTENT $data;");
+
+    let data: BTreeMap<String, Value> = [
+        ("post_type".into(), post_id.into()),
+        ("text".into(), model.text.clone().into()),
+        ("images".into(), model.images),
+    ]
+    .into();
     HttpResponse::Ok().await.unwrap()
 }
--- backend/src/fileupload/upload_sc.rs
+++ backend/src/fileupload/upload_sc.rs
@@ -10,21 +10,25 @@ pub enum PostType {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Model {
-    user_id: String,
-    post_type: PostType,
-    text: Option<String>,
-    images: Option<Vec<String>>,
-    videos: Option<Vec<String>>,
+pub struct Model<'a> {
+    pub user_token: String,
+    pub post_type: PostType,
+    pub text: Option<String>,
+    pub images: Option<Vec<&'a str>>,
+    pub videos: Option<Vec<String>>,
 }
 
 pub fn post_scope() -> Scope {
     web::scope("/post").route("{posty}", web::post().to(post_handle))
 }
 
-pub async fn post_handle(body: web::Json<Model>, posty: web::Path<String>) -> HttpResponse {
+pub async fn post_handle(
+    body: web::Json<Model>,
+    posty: web::Path<String>,
+    secret: web::Data<String>,
+) -> HttpResponse {
     match posty.as_str() {
-        "post-post" => post(body).await,
+        "post-post" => post(body, secret).await,
         _ => HttpResponse::BadRequest().await.unwrap(),
     }
 }
