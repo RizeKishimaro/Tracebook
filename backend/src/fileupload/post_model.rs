a259582fbbe080dfa0f5a0e617b5021c2d2e9b5e
Linus Walker <w000alker@gmail.com>
2023-02-22 09:08:43 +0630

    more secure error handling and for post_model

--- backend/src/fileupload/post_model.rs
+++ backend/src/fileupload/post_model.rs
@@ -44,62 +44,63 @@ pub async fn post(
         ),
     };
 
-    if let Err(_) = data_def {
-        return HttpResponse::InternalServerError().json(Response {
-            message: "Error in Encryption I don't fucking know!".to_string(),
-        });
-    }
+    match data_def {
+        Ok(data_defd) => {
+            let data: BTreeMap<String, Value> = [
+                ("post_id".into(), post_id.into()),
+                ("post_type".into(), format!("{:?}", model.post_type).into()),
+                ("text".into(), model.text.clone().into()),
+                ("images".into(), match_links(model.images.clone()).into()),
+                ("videos".into(), match_links(model.videos.clone()).into()),
+                (
+                    "user_poster".into(),
+                    format!("user:{}", user_info.unwrap().claims.id).into(),
+                ),
+            ]
+            .into();
 
-    let data: BTreeMap<String, Value> = [
-        ("post_id".into(), post_id.into()),
-        ("post_type".into(), format!("{:?}", model.post_type).into()),
-        ("text".into(), model.text.clone().into()),
-        ("images".into(), match_links(model.images.clone()).into()),
-        ("videos".into(), match_links(model.videos.clone()).into()),
-        (
-            "user_poster".into(),
-            format!("user:{}", user_info.unwrap().claims.id).into(),
-        ),
-    ]
-    .into();
+            let var: BTreeMap<String, Value> = [("data".into(), data.into())].into();
+            match ds.execute(&sql, ses, Some(var), false).await {
+                Ok(obj) => match get_value(obj) {
+                    Ok(v) => {
+                        let keys = vec![
+                            "post_id".to_string(),
+                            "post_type".to_string(),
+                            "text".to_string(),
+                            "images".to_string(),
+                            "videos".to_string(),
+                            "user_poster".to_string(),
+                        ];
 
-    let var: BTreeMap<String, Value> = [("data".into(), data.into())].into();
-    match ds.execute(&sql, ses, Some(var), false).await {
-        Ok(obj) => match get_value(obj) {
-            Ok(v) => {
-                let keys = vec![
-                    "post_id".to_string(),
-                    "post_type".to_string(),
-                    "text".to_string(),
-                    "images".to_string(),
-                    "videos".to_string(),
-                    "user_poster".to_string(),
-                ];
+                        let vec_str_resul = obj_str(v, keys);
 
-                let vec_str_resul = obj_str(v, keys);
+                        let user_sql = format!(
+                            "UPDATE {} SET posts += ['post:{}']",
+                            vec_str_resul[5], vec_str_resul[0]
+                        );
 
-                let user_sql = format!(
-                    "UPDATE {} SET posts += ['post:{}']",
-                    vec_str_resul[5], vec_str_resul[0]
-                );
-
-                match ds.execute(&user_sql, ses, None, false).await {
-                    Ok(_) => HttpResponse::Ok().json(ResponsePost {
-                        post_id: vec_str_resul[0].parse().unwrap_or(0o712404404),
-                        post_type: vec_str_resul[1].clone().into(),
-                        text: vec_str_resul[2].clone().into(),
-                        images: vec_str_resul[3].clone().into(),
-                        videos: vec_str_resul[4].clone().into(),
-                    }),
-                    Err(e) => HttpResponse::InternalServerError().json(Response {
-                        message: e.to_string(),
-                    }),
-                }
+                        match ds.execute(&user_sql, ses, None, false).await {
+                            Ok(_) => HttpResponse::Ok().json(ResponsePost {
+                                post_id: vec_str_resul[0].parse().unwrap_or(0o712404404),
+                                post_type: vec_str_resul[1].clone().into(),
+                                text: vec_str_resul[2].clone().into(),
+                                images: vec_str_resul[3].clone().into(),
+                                videos: vec_str_resul[4].clone().into(),
+                            }),
+                            Err(e) => HttpResponse::InternalServerError().json(Response {
+                                message: e.to_string(),
+                            }),
+                        }
+                    }
+                    Err(e) => HttpResponse::InternalServerError().json(Response { message: e }),
+                },
+                Err(e) => HttpResponse::InternalServerError().json(Response {
+                    message: e.to_string(),
+                }),
             }
-            Err(e) => HttpResponse::InternalServerError().json(Response { message: e }),
-        },
-        Err(e) => HttpResponse::InternalServerError().json(Response {
-            message: e.to_string(),
+        }
+        Err(_) => HttpResponse::InternalServerError().json(Response {
+            message: "I don't fucking know why!".to_string(),
         }),
     }
 }
